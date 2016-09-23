e7d4c9d0b7029f45be5d5e0cb4c12aaa1af09930
Christoph Höller <christoph.hoeller@googlemail.com>
2016-09-23 11:40:55 +0200

    Demonstrate protractor problem when using socket.io

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
 
+declare var io: any;
+
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+
+    socket: any;
+    constructor() {
+       this.socket = io('http://doesnt.matter');
+      // this.socket.disconnect(); //comment this line in and in works again!
+    }
+}
